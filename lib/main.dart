099f92c6ae1fe2d3cf4486b242d78d809ef6b84d
Nathan_ar <nathan.ar.94@gmail.com>
2023-10-26 22:34:33 -0300

    contador layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,95 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Contador());
+  runApp(const MyApp());
 }
 
-class Contador extends StatelessWidget {
-  const Contador({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Container(
-        color: Colors.white,
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  static void increment() {
+    print("increment");
+  }
+
+  static void decrement() {
+    print("decrement");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            "Contador",
+            style: TextStyle(
+              fontSize: 35,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(32),
+            child: Text(
+              '0',
+              style: TextStyle(
+                fontSize: 100,
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: increment,
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  fixedSize: const Size(50, 50),
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(7)),
+                ),
+                child: const Text(
+                  "+",
+                  style: TextStyle(
+                    fontSize: 28,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 25,
+              ),
+              TextButton(
+                onPressed: decrement,
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  fixedSize: const Size(50, 50),
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(7)),
+                ),
+                child: const Text(
+                  "-",
+                  style: TextStyle(
+                    fontSize: 28,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
